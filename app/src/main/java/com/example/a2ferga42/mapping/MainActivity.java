9d416fce2b42f82b4d8d660bc5cfedd42d23c07c
Aaron Ferguson <2ferga42@solent.ac.uk>
2018-01-29 12:36:56 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2ferga42/mapping/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a2ferga42.mapping;
+
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        MapView mv = (MapView) findViewById(R.id.map1);
+        mv.setBuiltInZoomControls(true);
+
+        mv.getController().setZoom(16);
+
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+    }
+}
